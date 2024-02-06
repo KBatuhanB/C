337464746b9ca3e5342235b9d765fac252ed9005
K.Batuhan Bölükbaşı <128256874+KBatuhanB@users.noreply.github.com>
2024-02-07 02:31:39 +0300

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using Business.Concrete;
+using Entitites.Concrete;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Globalization;
+
+
+Person person = new Person();
+person.ID = 12345678910;
+person.FirstName = "KELAMİ BATUHAN";
+person.LastName = "BÖLÜKBAŞI";
+person.DateOfBirthYear = 2004;
+
+
+PttManager pttManager = new PttManager(new PersonManager());
+pttManager.GiveMask((person));
