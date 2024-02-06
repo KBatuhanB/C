f027d5a35f805a844e11b70c5976eae6138f7122
K.Batuhan Bölükbaşı <128256874+KBatuhanB@users.noreply.github.com>
2024-02-07 02:28:58 +0300

    Add files via upload

---
+++ Entitites/Concrete/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entitites.Concrete
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public long ID { get; set; }
+        public int DateOfBirthYear { get; set; }
+
+    }
+}
